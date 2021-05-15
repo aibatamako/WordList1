1340b0d2817ff56b84f2397cd80830d49584a455
Tamako Aiba <t.aiba@4nonome.com>
2021-05-15 12:57:21 +0900

    Initial Commit

---
+++ WordList1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordList1
+//
+//  Created by tmk_cheer on 2021/05/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
